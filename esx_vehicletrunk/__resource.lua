3cfb9e25d1533cd0891e8ca217e935d527fe959e
MrLumi <lumilesff@hotmail.com>
2018-03-19 16:32:54 +0000

    Update __resource.lua

--- esx_vehicletrunk/__resource.lua
+++ esx_vehicletrunk/__resource.lua
@@ -3,18 +3,19 @@ version '0.0.1'
 description 'ESX Vehicletrunk'
 
 client_scripts {
-    'config.lua',
-    'client/main.lua',
     'locales/br.lua',
     'locales/en.lua',
     'locales/fr.lua'
+    'config.lua',
+    'client/main.lua'
 }
 
 server_scripts {
-	'@mysql-async/lib/MySQL.lua',
-    'config.lua',
-    'server/main.lua',
+    '@mysql-async/lib/MySQL.lua',
     'locales/br.lua',
     'locales/en.lua',
     'locales/fr.lua'
+    'config.lua',
+    'server/main.lua',
+    
 }
